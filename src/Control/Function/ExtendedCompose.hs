4e8aad7f03b5b194b54d924bbc132128a7bbeab1
Brian Jaress <bjaress@gmail.com>
2017-11-07 22:14:36 -0800

    Slightly more intuitive version

--- src/Control/Function/ExtendedCompose.hs
+++ src/Control/Function/ExtendedCompose.hs
@@ -1,6 +1,7 @@
 module Control.Function.ExtendedCompose
     ( o
     , s
+    , os
 
     , pose
     , posse
@@ -17,34 +18,34 @@ module Control.Function.ExtendedCompose
     ) where
 
 pose :: (a -> b) -> (q -> a) -> (q -> b)
-pose = (.)
+pose = os
 
 posse :: (a -> b) -> (q -> r -> a) -> (q -> r -> b)
-posse = pose . s
+posse = os . s
 
 possse :: (a -> b) -> (q -> r -> s -> a) -> (q -> r -> s -> b)
-possse = pose . s . s
+possse = os . s . s
 
 
 poose :: (a -> b -> c) -> (q -> b) -> (a -> q -> c)
-poose = o $ pose
+poose = o $ os
 
 poosse :: (a -> b -> c) -> (q -> r -> b) -> (a -> q -> r -> c)
-poosse = o $ pose . s
+poosse = o $ os . s
 
 poossse :: (a -> b -> c) -> (q -> r -> s -> b) -> (a -> q -> r -> s -> c)
-poossse = o $ pose . s . s
+poossse = o $ os . s . s
 
 
 pooose :: (a -> b -> c -> d) -> (q -> c) -> (a -> b -> q -> d)
-pooose = o $ o $ pose
+pooose = o $ o $ os
 
 pooosse :: (a -> b -> c -> d) -> (q -> r -> c) -> (a -> b -> q -> r -> d)
-pooosse = o $ o $ pose . s
+pooosse = o $ o $ os . s
 
 pooossse :: (a -> b -> c -> d) -> (q -> r -> s -> c) ->
     (a -> b -> q -> r -> s -> d)
-pooossse = o $ o $ pose . s . s
+pooossse = o $ o $ os . s . s
 
 
 s :: (a -> b) -> (q -> a) -> (q -> b)
@@ -52,3 +53,6 @@ s = (.)
 
 o :: (a -> b -> c) -> (q -> a) -> (b -> q -> c)
 o = (flip .) . (.)
+
+os :: (a -> b) -> (q -> a) -> (q -> b)
+os = (.)
