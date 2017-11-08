f5396faede65eb407be97209c6246f99d628693f
Brian Jaress <bjaress@gmail.com>
2017-11-07 21:23:06 -0800

    incremental version

--- src/Control/Function/ExtendedCompose.hs
+++ src/Control/Function/ExtendedCompose.hs
@@ -1,5 +1,55 @@
 module Control.Function.ExtendedCompose
-    ( pose
+    ( o
+    , s
+
+    , pose
+    , posse
+    , possse
+
+    , poose
+    , poosse
+    , poossse
+
+    , pooose
+    , pooosse
+    , pooossse
+
     ) where
 
+pose :: (a -> b) -> (q -> a) -> (q -> b)
 pose = (.)
+
+posse :: (a -> b) -> (q -> r -> a) -> (q -> r -> b)
+posse = pose . s
+
+possse :: (a -> b) -> (q -> r -> s -> a) -> (q -> r -> s -> b)
+possse = posse . s
+
+
+poose :: (a -> b -> c) -> (q -> b) -> (a -> q -> c)
+poose = o $ pose
+
+poosse :: (a -> b -> c) -> (q -> r -> b) -> (a -> q -> r -> c)
+poosse = o $ posse
+
+poossse :: (a -> b -> c) -> (q -> r -> s -> b) -> (a -> q -> r -> s -> c)
+poossse = o $ possse
+
+
+pooose :: (a -> b -> c -> d) -> (q -> c) -> (a -> b -> q -> d)
+pooose = o $ poose
+
+pooosse :: (a -> b -> c -> d) -> (q -> r -> c) -> (a -> b -> q -> r -> d)
+pooosse = o $ poosse
+
+pooossse :: (a -> b -> c -> d) -> (q -> r -> s -> c) ->
+    (a -> b -> q -> r -> s -> d)
+pooossse = o $ poossse
+
+
+s :: (a -> b) -> (q -> a) -> (q -> b)
+s = (.)
+
+
+o :: (a -> b -> c) -> (q -> a) -> (b -> q -> c)
+o = (flip .) . (.)
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,4 +9,30 @@ main = hspecWith defaultConfig {configFastFail = True} specs
 specs :: Spec
 specs = do
     it "basic compose" $
-          (Com.pose succ succ) 3 `shouldBe` 5
+        (Com.pose succ succ) 3 `shouldBe` 5
+    it "one arg with two arg" $
+        (Com.posse succ (-)) 100 20 `shouldBe` 81
+    it "one arg with three arg" $
+        (Com.possse succ multiplySubtract) 10 10 20 `shouldBe` 81
+
+    it "two arg with one arg" $
+        (Com.poose (-) succ) 100 10 `shouldBe` 89
+    it "two arg with two arg" $
+        (Com.poosse (-) (/)) 100 15 3 `shouldBe` 95
+    it "two arg with three arg" $
+        -- 100 - (3 * 5 - 4)
+        (Com.poossse (-) multiplySubtract) 100 3 5 4 `shouldBe` 89
+
+    it "three arg with one arg" $
+        -- 10 * 10 - (succ 5)
+        (Com.pooose multiplySubtract succ) 10 10 5 `shouldBe` 94
+    it "three arg with two arg" $
+        -- 10 * 20 - (16 / 2)
+        (Com.pooosse multiplySubtract (/)) 10 20 16 2 `shouldBe` 192
+    it "three arg with three arg" $
+        -- 10 * 20 - (5 * 3 - 2)
+        (Com.pooossse multiplySubtract multiplySubtract) 10 20 5 3 2
+            `shouldBe` 187
+
+
+multiplySubtract a b c = a * b - c
