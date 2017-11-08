4488c3bffbae41e43fb612f5614344bfc26f183a
Brian Jaress <bjaress@gmail.com>
2017-11-07 21:28:53 -0800

    exploded version

--- src/Control/Function/ExtendedCompose.hs
+++ src/Control/Function/ExtendedCompose.hs
@@ -23,33 +23,32 @@ posse :: (a -> b) -> (q -> r -> a) -> (q -> r -> b)
 posse = pose . s
 
 possse :: (a -> b) -> (q -> r -> s -> a) -> (q -> r -> s -> b)
-possse = posse . s
+possse = pose . s . s
 
 
 poose :: (a -> b -> c) -> (q -> b) -> (a -> q -> c)
 poose = o $ pose
 
 poosse :: (a -> b -> c) -> (q -> r -> b) -> (a -> q -> r -> c)
-poosse = o $ posse
+poosse = o $ pose . s
 
 poossse :: (a -> b -> c) -> (q -> r -> s -> b) -> (a -> q -> r -> s -> c)
-poossse = o $ possse
+poossse = o $ pose . s . s
 
 
 pooose :: (a -> b -> c -> d) -> (q -> c) -> (a -> b -> q -> d)
-pooose = o $ poose
+pooose = o $ o $ pose
 
 pooosse :: (a -> b -> c -> d) -> (q -> r -> c) -> (a -> b -> q -> r -> d)
-pooosse = o $ poosse
+pooosse = o $ o $ pose . s
 
 pooossse :: (a -> b -> c -> d) -> (q -> r -> s -> c) ->
     (a -> b -> q -> r -> s -> d)
-pooossse = o $ poossse
+pooossse = o $ o $ pose . s . s
 
 
 s :: (a -> b) -> (q -> a) -> (q -> b)
 s = (.)
-
 
 o :: (a -> b -> c) -> (q -> a) -> (b -> q -> c)
 o = (flip .) . (.)
